015a875a896d5c3177ba6f2177b289d754ca441e
Gabriel <gabrielcoxomega@gmail.com>
2024-04-02 17:58:03 +0100

    refactor: break up corpus-fault finding into multiple functions

--- dictionary_building/dictionary.py
+++ dictionary_building/dictionary.py
@@ -1,11 +1,9 @@
 import json
 import logging
+from typing import List
 logger = logging.getLogger(__name__)
 
-def find_faults_in_corpus(path: str):
-    with open(path, 'r') as recipes_file:
-        json_list = list(recipes_file)
-    corpus = [json.loads(json_str) for json_str in json_list if json_str[:2] != "//"] 
+def check_sentence_counts(path: str, corpus: List[object]):
     for i, recipe in enumerate(corpus): 
         n_sentences_original = len(recipe['original_recipe'].split("."))
         n_sentences_translation = len(recipe['gode_cookery_translation'].split('.'))
@@ -13,10 +11,18 @@ def find_faults_in_corpus(path: str):
             logger.info(f"Inconsistent sentence counts | "
                         f"line {i+1} of {path} | "
                         f"{n_sentences_original} vs {n_sentences_translation}.")
+
+def find_faults_in_corpus(path: str):
+    with open(path, 'r') as recipes_file:
+        json_list = list(recipes_file)
+    corpus = [json.loads(json_str) for json_str in json_list if json_str[:2] != "//"] 
+    check_sentence_counts(path, corpus)
     
+
 def main():
     logging.basicConfig(filename='dictionary.log', level=logging.INFO)
     find_faults_in_corpus('resources/recipes.jsonl')
+
 
 if __name__ =='__main__':
     main()
