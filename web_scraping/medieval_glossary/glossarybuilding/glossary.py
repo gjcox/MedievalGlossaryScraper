20b73a46f38f9158fa9004e57a0cbdca48c43e1e
Gabriel <gabrielcoxomega@gmail.com>
2024-04-03 12:37:51 +0100

    feat: add GlossaryEntry and Meaning classes

---
+++ web_scraping/medieval_glossary/glossarybuilding/glossary.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Meaning:
+    substitution: str
+    note: str
+    def __init__(self, sub: str, note : str | None = None) -> None:
+        self.substitution = sub.strip(" .")
+        if note:
+            self.note = note.strip(" .")
+
+class GlossaryEntry:
+    plaintext: str
+    meanings: List[Meaning]
+
+    def __init__(self, plaintext: str) -> None:
+        self.plaintext = plaintext.strip()
+        self.meanings = []
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return f"{self.plaintext} - {"; ".join(map(lambda m: m.substitution, self.meanings))}"
+
+    def add_meaning(self, meaning: Meaning) -> None:
+        self.meanings.append(meaning)
