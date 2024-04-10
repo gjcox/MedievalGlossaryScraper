c167fc77b9b5e930d5a5a1a23f44d5e767e3e273
Gabriel <gabrielcoxomega@gmail.com>
2024-04-10 15:30:47 +0100

    feat: implement glossary sorting

---
+++ glossary_tools/edit_glossary.py
@@ -0,0 +1,45 @@
+import sys, os, argparse, json, io
+
+''' I am aware this is quite hacky, but to my understanding scrapy makes
+    temporary virtual filespaces when it runs, so it is easier to jankily
+    import modules from the web_scraping package than into the web_scraping
+    package. Additionally, while this might be a brittle solution I only 
+    intend this code to run on my machine so flexibility is not a high 
+    priority. '''
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import web_scraping.medieval_glossary.glossarybuilding.glossary as glossary
+import web_scraping.medieval_glossary.glossarybuilding.util as glosutils
+
+tasks = {
+    'alpha': 'alphabetise'
+}
+
+def alphabetise(source: io.TextIOWrapper, output: io.TextIOWrapper):
+    json_list = list(source)
+    entry_list = list(map(json.loads, json_list))
+    entry_list.sort(key=lambda entry : entry['plaintext'])
+    output.writelines("\n".join(
+        map(lambda x: json.dumps(x, ensure_ascii=False), entry_list)))
+
+def main():
+    parser = argparse.ArgumentParser(description='Process a JSON-lines glossary file.')
+    parser.add_argument("task", choices=tasks.values())
+    parser.add_argument("source", type=argparse.FileType('r', encoding='UTF-8'), 
+                        help="the current glossary file")
+    parser.add_argument("output", type=argparse.FileType('w', encoding='UTF-8'),
+                        help="the file to write the processed version of the glossary to")
+    args = parser.parse_args()
+
+    if args.task == tasks['alpha']:
+        alphabetise(args.source, args.output)
+    else:
+        parser.print_help()
+
+    args.source.close()
+    args.output.close()
+
+
+if __name__ == "__main__":
+    main()
+
+    
